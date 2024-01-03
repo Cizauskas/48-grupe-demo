ee3f5c1d4dbb350fc511c29b24be291cbdbcb7f5
Cizauskas <153712091+Cizauskas@users.noreply.github.com>
2024-01-03 10:05:42 +0200

    Arrow.js

---
+++ functions/arrow.js
@@ -0,0 +1,17 @@
+const n1 = 7;
+const n2 = 5;
+
+
+function sum(a, b){    // fabrikėlis 
+    return a + b;
+}
+
+console.log (`${n1}+${n2} = ${sum(n1, n2)}`); 
+
+function minus(a, b){    // fabrikėlis 
+    return a - b;
+}
+
+console.log (`${n1}-${n2} = ${minus(n1, n2)}`); 
+
+
