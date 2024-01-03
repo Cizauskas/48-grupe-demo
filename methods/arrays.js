a509344bae95e0f8b979b680d6a97db0503bbe72
Cizauskas <153712091+Cizauskas@users.noreply.github.com>
2024-01-03 09:21:33 +0200

    Methods

---
+++ methods/arrays.js
@@ -0,0 +1,32 @@
+const marks = [10,2,8,4,6,];
+
+console.log (marks[0]);
+console.log (marks[1]);
+console.log (marks[2]);
+console.log (marks[3]);
+console.log (marks[4]);
+console.log (marks[7777]);
+
+console.log(marks[marks.length - 1], marks.at(-1));
+console.log(marks[marks.length - 2], marks.at(-2));
+console.log(marks[marks.length - 3], marks.at(-3));
+console.log(marks[marks.length - 4], marks.at(-4));
+console.log(marks[marks.length - 5], marks.at(-5));
+
+const a = [1,1,1];
+
+
+console.log('--------------------------');
+
+console.log(marks.join());
+console.log(marks.join(', '));
+
+
+const x = 102846;
+
+const x1 = marks.join();
+
+
+
+
+
---
+++ methods/strings.js
@@ -0,0 +1,30 @@
+//at 
+//includes
+//Indexof
+//edswitch
+//chart atcharcodeat
+//fromChartcode
+//.repeat
+//slice
+// replace all
+// split 
+//upper lower case
+//Trim 
+
+
+//console.log('PoMiDoras').toLowerCase());
+//console.log('PoMiDoras').toUpperCase());
+
+//const pomBad = 'PoMiDoras' ; 
+
+//function makeItGood(text) {
+   /// return text {0}.toUpperCase () + text.slice(1).toLowerCase;
+//}
+
+
+console.log(`"${'labas'.trim()}"`); 
+console.log(`"${'labas'.trimStart()}"`); 
+console.log(`"${'labas'.trimEnd()}"`); 
+
+
+
