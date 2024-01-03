08f7bbdb36816c0e3d4ea862a4759c2d64b2f7df
Cizauskas <153712091+Cizauskas@users.noreply.github.com>
2024-01-03 19:37:02 +0200

    empty

--- functions/empty.js
+++ functions/empty.js
@@ -1,26 +1,24 @@
 /*
-FUNCTION- funkcija 
-perpanaudojimas logikos blokas
+FUNCTION - funkcija
+perpanaudojamas logikos blokas
 */
 
 function trys() {
     return 3;
 }
 
-
 function penki() {
-    // funkcijos logikos blokas 
+    // funkcijos logikos blokas
     return 5;
 }
 
 const ats1 = penki();
 console.log(ats1);
 
-const ats2 = penki();
+const ats2 = trys();
 console.log(ats2);
 
-
-const ats3 = trys();
+const ats3 = penki();
 console.log(ats3);
 
 const ats4 = trys();
@@ -29,13 +27,24 @@ console.log(ats4);
 console.log(penki());
 console.log(trys());
 
-function pomidoras () {
-    console.log('Va, suveike pomidoras...')
-    return 123; // kol kas tai beprasme reiksme
+function kazkasNaujo() {
+    777;
+    return undefined;
 }
 
-pomidoras (); // nes nieko su ja nedarome 
+console.log(kazkasNaujo());
 
+
+function pomidoras() {
+    console.log('Va, suveike pomidoras...');
+    return 123;     // kol kas tai kaip beprasme reiksme
+}
+
+pomidoras();        // nes nieko su ja nedarome
+pomidoras();
+pomidoras();
+pomidoras();
+pomidoras();
 
 
 
